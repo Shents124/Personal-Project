c7147183c6205bd52a5f5e5c90d60e6de20c9db5
Shents124 <72143563+Shents124@users.noreply.github.com>
2021-06-23 16:18:22 +0700

    Create PlayerLife script to control health of player

---
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    private int currentHealth;
+    private int maxHealth = 200;
+    public HealthBar healthBar;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Animal"))
+        {
+            TakeDame();
+        }
+    }
+
+    private void TakeDame()
+    {
+        currentHealth -= 5;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Game Over");
+        }
+    }
+}
