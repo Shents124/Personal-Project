fcdcd1d46790bf3088d3e72f103b50ef8a76cc20
Shents124 <72143563+Shents124@users.noreply.github.com>
2021-06-23 16:43:30 +0700

    add Action GameOver to call when player die

--- Assets/Scripts/EventBroker.cs
+++ Assets/Scripts/EventBroker.cs
@@ -4,16 +4,21 @@ public class EventBroker
 {
     public static Action<int> UpdateScore;
     public static Action UpdateCountAnimal;
+    public static Action GameOver;
     public static Action<WeaponFoodType> PickupWeaponFood;
 
     public static void CallUpdateScore(int score)
     {
         UpdateScore?.Invoke(score);
     }
-
     public static void CallUpdateCountAnimal()
     {
         UpdateCountAnimal?.Invoke();
+    }
+
+    public static void CallGameOver()
+    {
+        GameOver?.Invoke();
     }
     public static void CallPickupWeaponFood(WeaponFoodType weaponFoodType)
     {
